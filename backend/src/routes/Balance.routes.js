da8abe713a087ac851bf1ecd3db79f4e5a348eb4
Aryan Yadav <aaryany2505@gmail.com>
2026-06-24 23:09:45 +0530

    added AccountRouter

---
+++ backend/src/routes/Balance.routes.js
@@ -0,0 +1,4 @@
+import express from "express";
+
+const BalanceRouter = express.Router()
+export default BalanceRouter
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import UserRouter from "./User.routes.js"
+import BalanceRouter from "./Balance.routes.js"
 export const Router = express.Router()
 Router.use("/user",UserRouter)
+Router.use("/account",BalanceRouter)
 
