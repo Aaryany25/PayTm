b9685a413bfd475374c825fb9964a344d64eba13
Aryan Yadav <aaryany2505@gmail.com>
2026-08-20 22:23:53 +0530

    update the UI

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -58,7 +58,7 @@ function SendMoney() {
                             }
                         })
                         // console.log("transfered")
-                    }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                    }} class="justify-center rounded-lg text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
                 </div>
